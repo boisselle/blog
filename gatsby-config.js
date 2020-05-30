efb1c3674239faaa53c10cbaeeff2b60dc7f052d
SEAN <augmentedsean@gmail.com>
2020-05-30 02:20:08 -0700

    changed blog title and updated twitter link

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
+    title: `Sean's Developer Blog`,
+    author: `Sean Boisselle`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `_boisselle`,
     },
   },
   plugins: [
